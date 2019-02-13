b101ead2ddf91d6ec11086326e80af62c732f9eb
pprg1996 <pprg1996@gmail.com>
2019-02-13 05:10:21 -0400

    All hover menus working

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-document.querySelector(".projectMenuButton").addEventListener("click", e => {
-  e.preventDefault();
-  const hoverMenu = document.querySelector(".projectImgHoverDiv");
-  const buttonIcon = document.querySelector(".projectMenuButtonIcon");
+document.querySelectorAll(".projectMenuButton").forEach((menuButton) => {
+  menuButton.addEventListener("click", e => {
+    e.preventDefault();
+    const hoverMenu = menuButton.parentElement.querySelector(".projectImgHoverDiv");
+    const buttonIcon = menuButton.querySelector(".projectMenuButtonIcon");
 
-  if (!hoverMenu.classList.contains("show")) {
-    buttonIcon.classList.replace("fa-bars", "fa-window-close");
+    if (!hoverMenu.classList.contains("show")) {
+      buttonIcon.classList.replace("fa-bars", "fa-window-close");
 
-    hoverMenu.classList.toggle("showDisplay");
-    setTimeout(() => {
-      hoverMenu.classList.toggle("show");
-    }, 1);
-  } else {
-    buttonIcon.classList.replace("fa-window-close", "fa-bars");
-
-    hoverMenu.classList.toggle("show");
-    setTimeout(() => {
       hoverMenu.classList.toggle("showDisplay");
-    }, 200);
-  }
+      setTimeout(() => {
+        hoverMenu.classList.toggle("show");
+      }, 1);
+    } else {
+      buttonIcon.classList.replace("fa-window-close", "fa-bars");
+
+      hoverMenu.classList.toggle("show");
+      setTimeout(() => {
+        hoverMenu.classList.toggle("showDisplay");
+      }, 200);
+    }
+  });
 });
